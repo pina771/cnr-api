079677e6314b9987e25f508c7b26f1a96ecd6074
pina771 <filip.katarina@gmail.com>
2022-05-23 19:34:05 +0200

    removed morgan

--- src/main.ts
+++ src/main.ts
@@ -1,6 +1,5 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
-import * as morgan from 'morgan';
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   app.enableCors();
